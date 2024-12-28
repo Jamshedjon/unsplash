474f7ee2247616d137b62dd308dabd47d41b1105
Jamshedjon Hasanjonov <136963308+Jamshedjon@users.noreply.github.com>
2024-12-28 14:58:52 +0500

    Update tailwind.config.js
    change plugin import

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
+import typography from '@tailwindcss/typography';
+import daisyui from 'daisyui';
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+  plugins: [typography, daisyui],
 };
